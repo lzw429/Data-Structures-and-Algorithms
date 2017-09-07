dddfaf57c66ff0277d048ae61decf71c9a38ab62
Yiheng Shu <lzw429@qq.com>
2017-09-07 19:46:41 +0800

    update

--- LinearList/linearList.cpp
+++ LinearList/linearList.cpp
@@ -174,3 +174,17 @@ void arrayList<T>::insert(int theIndex,const T& theElement)
 	element[theIndex] = theElement;
 	listSize++;
 }
+
+//把一个线性表插入输出流
+template<class T>
+void arrayList<T>::output(cout->out)const
+{
+	//把线性表插入输出流
+	copy(element, element + listSize, ostream_iterator<T>(cout, " "));
+}
+//重载流插入符<<
+template<class T>
+ostream &operator<< (ostream &out,const arrayList<T> &x)
+{
+	x.output(out); return out;
+}
