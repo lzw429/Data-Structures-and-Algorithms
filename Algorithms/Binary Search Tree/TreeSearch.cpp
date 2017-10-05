ea5ee84810134b51984eaddee093f476310b481d
Yiheng Shu <lzw429@qq.com>
2017-10-05 21:34:19 +0800

    update

---
+++ Algorithms/Binary Search Tree/TreeSearch.cpp
@@ -0,0 +1,16 @@
+class BinarySearchTree
+{
+private:
+	int key;
+	BinarySearchTree *left;
+	BinarySearchTree *right;
+};
+void TreeSearch(BinarySearchTree *x, int k)
+{
+	if (x == nullptr || k = x->key)
+		return x;
+	if (k < x->key)
+		return TreeSearch(x->left, k);
+	else
+		return TreeSearch(x->right, k);
+}
