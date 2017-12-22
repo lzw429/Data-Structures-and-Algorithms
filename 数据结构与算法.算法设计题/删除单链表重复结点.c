e5371477c64cabf12aa3563340670cf2fd14cd71
Yiheng Shu <lzw429@qq.com>
2017-12-22 08:57:44 +0800

    update

--- 数据结构与算法.算法设计题/删除单链表重复结点.c
+++ 数据结构与算法.算法设计题/删除单链表重复结点.c
@@ -5,9 +5,19 @@ struct ListNode {
 };
 
 void del_redundant(ListNode *&head) {
-    for (ListNode *p = head; p != nullptr; p = p->next) {
-        for (ListNode *q = p->next; q != nullptr; q = q->next) {
-            if (p->val == q->val)
+    ListNode *p, *q, *s;
+    for (ListNode *p = head->next; p != nullptr; p = p->next) {
+        for (ListNode *q = p->next, s = p; q != nullptr;) {
+            if (p->val == q->val)// q是多余结点
+            {
+                s.next = q->next;// s仅作为临时变量
+                free(q);
+                q = s.next;
+            } else// q不是多余结点
+            {
+                s = q;//使得s是q前一个结点
+                q = q->next;
+            }
         }
     }
 }
