657b9bc5b6a6b6dd13d2da85c9b5a2891b5d094d
Yiheng Shu <lzw429@qq.com>
2018-01-12 20:35:48 +0800

    update

--- Algorithms/Sorting/InsertionSort.cpp
+++ Algorithms/Sorting/InsertionSort.cpp
@@ -6,8 +6,8 @@ void InsertionSort(int arr[], int p, int r)
 	for (int j = 1; j <= r; ++j)
 	{
 		key = arr[j];
-		//将arr[j]插入到已排序的arr[0...j-1]
-		int i = j - 1;//j之前已排好序
+		// 将arr[j]插入到已排序的arr[0...j-1]
+		int i = j - 1;// j之前已排好序
 		while (i >= p && arr[i] > key)
 		{
 			arr[i + 1] = arr[i];
--- Algorithms/Sorting/SelectionSort.cpp
+++ Algorithms/Sorting/SelectionSort.cpp
@@ -2,10 +2,10 @@ void SelectionSort(int arr[], int p, int r)
 {
 	for (int i = p; i < r; ++i)
 	{
-		int min = i;
+		int min = i; // i之前的数是有序的
 		for (int j = i + 1; j < r; j++)
 		{
-			if (arr[j] < arr[min])
+			if (arr[j] < arr[min]) // 选出无序部分最小的数
 				min = j;
 		}
 		swap(arr[i], arr[min]);
