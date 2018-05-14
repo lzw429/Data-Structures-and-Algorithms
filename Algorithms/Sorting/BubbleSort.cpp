b2359b33a4c54694a25707d6139c4e46f2df45c2
Yiheng Shu <lzw429@qq.com>
2018-05-14 23:04:09 +0800

    update

--- Algorithms/Sorting/BubbleSort.cpp
+++ Algorithms/Sorting/BubbleSort.cpp
@@ -2,13 +2,11 @@ void BubbleSort(int arr[], int p, int r) {
     int i, j;
     bool sorted = true;
     for (i = p; i < r; ++i) {
-		sorted = true;
+        sorted = true;
         for (j = p; j < p + r - i - 1; ++j) //内层循环每次使最大数移到后面
         {
             if (arr[j] > arr[j + 1]) {
-                int t = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = t;
+                swap(arr[j], arr[j + 1]);
                 sorted = false;
             }
         }
